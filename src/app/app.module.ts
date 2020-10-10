dec402d08fbce697162a146ed87ba73135b9bdcb
ChiaraDipi <dipi.chiara@gmail.com>
2020-10-10 17:39:06 +0200

    Add VersesGroupComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -51,6 +51,7 @@ import { SuppliedComponent } from './components/supplied/supplied.component';
 import { SurplusComponent } from './components/surplus/surplus.component';
 import { TextComponent } from './components/text/text.component';
 import { VerseComponent } from './components/verse/verse.component';
+import { VersesGroupComponent } from './components/verses-group/verses-group.component';
 import { EditorialConventionLayoutDirective } from './directives/editorial-convention-layout.directive';
 import { HighlightDirective } from './directives/highlight.directive';
 import { HtmlAttributesDirective } from './directives/html-attributes.directive';
@@ -142,6 +143,7 @@ export function initializeApp(appConfig: AppConfig) {
     TextTextComponent,
     TextVersionsComponent,
     VerseComponent,
+    VersesGroupComponent,
     VersionPanelComponent,
     WitnessPanelComponent,
     XmlBeautifyPipe,
