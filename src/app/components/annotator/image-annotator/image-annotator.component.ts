686908b44cef75abcb725151d936a198f5e4b961
giacomocerre <cerre.giacomo93@gmail.com>
2020-10-20 17:58:22 +0200

    Add image annotation anchoring

--- src/app/components/annotator/image-annotator/image-annotator.component.ts
+++ src/app/components/annotator/image-annotator/image-annotator.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AnnotatorService } from 'src/app/services/annotator/annotator.service';
 import * as Annotorious from '@recogito/annotorious-openseadragon';
+import { Annotation, AnnotationID } from 'src/app/models/evt-models';
+import { IdbService } from 'src/app/services/idb.service';
 
 @Component({
   selector: 'evt-image-annotator',
@@ -8,27 +10,54 @@ import * as Annotorious from '@recogito/annotorious-openseadragon';
   styleUrls: ['./image-annotator.component.scss']
 })
 export class ImageAnnotatorComponent implements OnInit {
-  public startAnnotation: boolean = false;
   public anno;
+  public annotation;
 
   constructor(
-    private annotator: AnnotatorService
+    private annotator: AnnotatorService,
+    private db: IdbService
   ) { }
 
   ngOnInit(): void {
     this.annotator.imageSelection.subscribe((viewer) => {
-       this.anno = Annotorious(viewer, {})
+      viewer
+       ? (this.anno = Annotorious(viewer, {}))
+       : null
+       this.anno.on('createAnnotation', (a:Annotation) => {
+         this.createAnnotation(a)
+      });
+      this.db.getAll().then((annotations: Array<AnnotationID>) => {
+        this.anno.setAnnotations(annotations);
+      });
     })
-  }
-
-  initializeImgAnnotation(){
-    this.startAnnotation = !this.startAnnotation
-    this.anno.setDrawingEnabled(this.startAnnotation)
+  
   }
 
   setDrawType(type){
     this.anno.setDrawingEnabled(true)
-    this.anno.setDrawingTool(type);
+    this.anno.setDrawingTool(type); 
+  }
+
+  createAnnotation(a) {
+    const annotation: Annotation = {
+      '@context': 'http:www.w3.org/ns/anno.jsonld',
+      id:a.id,
+      type:"Annotation",
+      created: new Date().toISOString(),
+      body: {
+        type:"TextualBody",
+        value:a.body.value,
+        format:"text/html",
+        purpose: a.body[0].purpose
+      },
+      target: {
+        source: a.target.source,
+        type:"image",
+        selector: [a.target.selector]
+      }
+    }
+    this.annotator.addAnnotation(annotation)
+    
   }
 
 }
